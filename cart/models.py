2f7a61528d67efdbced4370a201a52e8fa070458
takashi00b2904 <takashi.dayo.0728@gmail.com>
2020-02-29 11:57:07 +0900

    lesson08

--- cart/models.py
+++ cart/models.py
@@ -8,6 +8,6 @@ User = settings.AUTH_USER_MODEL
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
-    total = models.DecimalField(default=0.00, max_digits=9, decimal_places=2) 1, 000, 000.00
+    total = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
